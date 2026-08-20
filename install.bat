aad1df0af8d80ac04b54cae51efd5af13121d1a7
Sophiedevops <163428008+Sophiedevops@users.noreply.github.com>
2026-08-20 14:09:49 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,19 +1,15 @@
 @echo off
-chcp 65001 >nul
 setlocal
 
-echo === Установка Cherry Proxy Lite ===
-set /p CF_ACCOUNT_ID="1. Введите ваш Account ID: "
-set /p CF_API_TOKEN="2. Введите ваш API Token: "
+echo === Cherry Proxy Lite Installer ===
+set /p CF_ACCOUNT_ID="1. Enter your Account ID: "
+set /p CF_API_TOKEN="2. Enter your API Token: "
 set WORKER_NAME=cherry-proxy-lampa
 
 echo.
-echo Отправка скрипта в Cloudflare...
-curl -s -X PUT "https://api.cloudflare.com/client/v4/accounts/%CF_ACCOUNT_ID%/workers/scripts/%WORKER_NAME%" ^
-     -H "Authorization: Bearer %CF_API_TOKEN%" ^
-     -H "Content-Type: application/javascript" ^
-     --data-binary @worker.js
+echo Deploying script to Cloudflare...
+curl -s -X PUT "https://api.cloudflare.com/client/v4/accounts/%CF_ACCOUNT_ID%/workers/scripts/%WORKER_NAME%" -H "Authorization: Bearer %CF_API_TOKEN%" -H "Content-Type: application/javascript" --data-binary @worker.js
 
 echo.
-echo === Готово! ===
+echo === Done! ===
 pause
